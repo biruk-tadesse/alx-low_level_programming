7a6b8bf35599ab42b6bb846928a5fd1e1981349a
biruk-tadesse <biruk.tadesse.tefera@gmail.com>
2022-07-13 19:06:15 +0000

    101-make_me_win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/biruk-tadesse/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/biruk-tadesse/alx-low_level_programming/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/nrandom.so
